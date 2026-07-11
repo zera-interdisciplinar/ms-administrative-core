621d5677772f142815d5553d0e4b25c324974813
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2026-07-11 15:38:25 -0300

    feat: refactor UserRepository interface to improve user data operations

--- src/main/java/com/zera/ms_administrative_core/core/repository/UserRepository.java
+++ src/main/java/com/zera/ms_administrative_core/core/repository/UserRepository.java
@@ -8,10 +8,10 @@ import com.zera.ms_administrative_core.core.domain.entity.User;
 import com.zera.ms_administrative_core.core.domain.valueobject.Email;
 
 public interface UserRepository {
-      User insert(User user);
-      User delete(User user);
-      User update(User user);
-      Optional<User> findById(UUID id);
-      Optional<User> findByEmail(Email email);
-      List<User> findAll();
+    void save(User user);
+    void delete(UUID id);
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(Email email);
+    List<User> findAll(int page, int size);
+    boolean existsByEmail(Email email);
 }
