320fbe761ec5f7da456a4e3e2ff1c4d83d74be64
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2026-07-15 14:36:56 -0300

    feat: implement RegisterUser use case with command and output models

---
+++ src/main/java/com/zera/ms_administrative_core/core/usecase/register/RegisterUserOutput.java
@@ -0,0 +1,13 @@
+package com.zera.ms_administrative_core.core.usecase.register;
+
+import java.util.UUID;
+
+import com.zera.ms_administrative_core.core.domain.entity.Role;
+import com.zera.ms_administrative_core.core.domain.valueobject.Email;
+
+public record RegisterUserOutput(
+    UUID userId,
+    String name,
+    Email email,
+    Role role
+) {}
