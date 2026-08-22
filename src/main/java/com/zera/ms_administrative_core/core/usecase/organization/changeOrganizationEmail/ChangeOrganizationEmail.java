1ed2b6628cc41fb22ba76a53ad972660127b4535
aoas2009 <aoasilva122009@hotmail.com>
2026-08-22 19:49:21 -0300

    feat: criando usecase ChangeOrganizationEmail

---
+++ src/main/java/com/zera/ms_administrative_core/core/usecase/organization/changeOrganizationEmail/ChangeOrganizationEmail.java
@@ -0,0 +1,9 @@
+package com.zera.ms_administrative_core.core.usecase.organization.changeOrganizationEmail;
+
+import com.zera.ms_administrative_core.core.domain.valueobject.Email;
+
+import java.util.UUID;
+
+public interface ChangeOrganizationEmail {
+    void execute(UUID organizationId, Email email);
+}
---
+++ src/main/java/com/zera/ms_administrative_core/core/usecase/organization/changeOrganizationEmail/ChangeOrganizationEmailImpl.java
@@ -0,0 +1,26 @@
+package com.zera.ms_administrative_core.core.usecase.organization.changeOrganizationEmail;
+
+import com.zera.ms_administrative_core.core.domain.entity.Organization;
+import com.zera.ms_administrative_core.core.domain.exception.OrganizationNotFoundException;
+import com.zera.ms_administrative_core.core.domain.valueobject.Email;
+import com.zera.ms_administrative_core.core.repository.OrganizationRepository;
+
+import java.util.UUID;
+
+public class ChangeOrganizationEmailImpl implements ChangeOrganizationEmail {
+
+    private final OrganizationRepository repository;
+
+    public ChangeOrganizationEmailImpl(OrganizationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void execute(UUID organizationId, Email email) {
+        Organization org = repository.findById(organizationId)
+                .orElseThrow(() -> new OrganizationNotFoundException(organizationId));
+
+        org.changeEmail(email);
+        repository.save(org);
+    }
+}
