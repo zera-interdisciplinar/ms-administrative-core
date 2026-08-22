0e85f68a4ce5d541279771acb35380f2d7fcfebb
aoas2009 <aoasilva122009@hotmail.com>
2026-08-22 19:45:40 -0300

    feat: adicionando Optional para métodos das interfaces de Organization e Telephone

--- src/main/java/com/zera/ms_administrative_core/core/repository/OrganizationRepository.java
+++ src/main/java/com/zera/ms_administrative_core/core/repository/OrganizationRepository.java
@@ -5,12 +5,13 @@ import com.zera.ms_administrative_core.core.domain.valueobject.Cnpj;
 import com.zera.ms_administrative_core.core.domain.valueobject.Email;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrganizationRepository {
     Organization save(Organization organization);
-    Organization findById(UUID id);
-    Organization findByCnpj(Cnpj cnpj);
+    Optional<Organization> findById(UUID id);
+    Optional<Organization> findByCnpj(Cnpj cnpj);
     boolean existsByCnpj(Cnpj cnpj);
     boolean existsByEmail(Email email);
     List<Organization> findAll();
--- src/main/java/com/zera/ms_administrative_core/core/repository/TelephoneRepository.java
+++ src/main/java/com/zera/ms_administrative_core/core/repository/TelephoneRepository.java
@@ -3,13 +3,14 @@ package com.zera.ms_administrative_core.core.repository;
 import com.zera.ms_administrative_core.core.domain.entity.Telephone;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TelephoneRepository {
     Telephone save(Telephone telephone);
-    Telephone findById(UUID id);
-    Telephone findByUserId(UUID id);
-    Telephone findByRecyclingBusinessId(UUID id);
+    Optional<Telephone> findById(UUID id);
+    Optional<Telephone> findByUserId(UUID id);
+    Optional<Telephone> findByRecyclingBusinessId(UUID id);
 
     void delete(Telephone telephone);
     List<Telephone> findAll();
