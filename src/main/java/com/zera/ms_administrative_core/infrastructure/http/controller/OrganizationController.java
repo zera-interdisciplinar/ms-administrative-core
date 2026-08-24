d03f20b5bc6f976a3d8f10ad581ab90b7cfbf225
aoas2009 <aoasilva122009@hotmail.com>
2026-08-24 20:56:52 -0300

    feat: criando Controller e JPA de Organization

---
+++ src/main/java/com/zera/ms_administrative_core/infrastructure/http/controller/OrganizationController.java
@@ -0,0 +1,54 @@
+package com.zera.ms_administrative_core.infrastructure.http.controller;
+
+import com.zera.ms_administrative_core.core.usecase.organization.activateOrganization.ActivateOrganization;
+import com.zera.ms_administrative_core.core.usecase.organization.changeOrganizationEmail.ChangeOrganizationEmail;
+import com.zera.ms_administrative_core.core.usecase.organization.deactivateOrganization.DeactivateOrganization;
+import com.zera.ms_administrative_core.core.usecase.organization.findOrganization.FindAllOrganizations;
+import com.zera.ms_administrative_core.core.usecase.organization.findOrganization.FindOrganizationByCnpj;
+import com.zera.ms_administrative_core.core.usecase.organization.findOrganization.FindOrganizationByEmail;
+import com.zera.ms_administrative_core.core.usecase.organization.findOrganization.FindOrganizationById;
+import com.zera.ms_administrative_core.core.usecase.organization.registerOrganization.RegisterOrganization;
+import com.zera.ms_administrative_core.core.usecase.organization.renameOrganization.RenameOrganization;
+import com.zera.ms_administrative_core.core.usecase.organization.suspendOrganization.SuspendOrganization;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/organization")
+public class OrganizationController {
+    private final FindAllOrganizations findAllOrganizations;
+    private final RegisterOrganization registerOrganization;
+    private final FindOrganizationById findOrganizationById;
+    private final FindOrganizationByCnpj findOrganizationByCnpj;
+    private final FindOrganizationByEmail findOrganizationByEmail;
+    private final RenameOrganization renameOrganization;
+    private final ChangeOrganizationEmail changeOrganizationEmail;
+    private final ActivateOrganization activateOrganization;
+    private final DeactivateOrganization deactivateUser;
+    private final SuspendOrganization suspendOrganization;
+
+    public OrganizationController(
+            FindAllOrganizations findAllOrganizations,
+            RegisterOrganization registerOrganization,
+            FindOrganizationById findOrganizationById,
+            FindOrganizationByCnpj findOrganizationByCnpj,
+            FindOrganizationByEmail findOrganizationByEmail,
+            RenameOrganization renameOrganization,
+            ChangeOrganizationEmail changeOrganizationEmail,
+            ActivateOrganization activateOrganization,
+            DeactivateOrganization deactivateUser,
+            SuspendOrganization suspendOrganization) {
+
+        this.findAllOrganizations = findAllOrganizations;
+        this.registerOrganization = registerOrganization;
+        this.findOrganizationById = findOrganizationById;
+        this.findOrganizationByCnpj = findOrganizationByCnpj;
+        this.findOrganizationByEmail = findOrganizationByEmail;
+        this.renameOrganization = renameOrganization;
+        this.changeOrganizationEmail = changeOrganizationEmail;
+        this.activateOrganization = activateOrganization;
+        this.deactivateUser = deactivateUser;
+        this.suspendOrganization = suspendOrganization;
+    }
+}
