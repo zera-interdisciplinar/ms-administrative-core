e0658de6253a7fdd075d3d9bb87202a226b145d1
aoas2009 <aoasilva122009@hotmail.com>
2026-08-29 17:07:22 -0300

    feat: Criando TelephoneController com metodo GET base (findAll)

---
+++ src/main/java/com/zera/ms_administrative_core/infrastructure/http/controller/TelephoneController.java
@@ -0,0 +1,56 @@
+package com.zera.ms_administrative_core.infrastructure.http.controller;
+
+import com.zera.ms_administrative_core.core.usecase.telephone.changeTelephone.ChangeTelephone;
+import com.zera.ms_administrative_core.core.usecase.telephone.deleteTelephone.DeleteTelephone;
+import com.zera.ms_administrative_core.core.usecase.telephone.findTelephone.*;
+import com.zera.ms_administrative_core.core.usecase.telephone.registerTelephone.RegisterRecyclingTelephone;
+import com.zera.ms_administrative_core.core.usecase.telephone.registerTelephone.RegisterUserTelephone;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/telephone")
+public class TelephoneController {
+    private final ChangeTelephone changeTelephone;
+    private final DeleteTelephone deleteTelephone;
+    private final FindAllTelephones findAllTelephones;
+    private final FindAllTelephonesByOrganizationId findAllTelephonesByOrganizationId;
+    private final FindTelephoneById findTelephoneById;
+    private final FindTelephoneByRecyclingBusinessId findTelephoneByRecyclingBusinessId;
+    private final FindTelephoneByUserId findTelephoneByUserId;
+    private final RegisterRecyclingTelephone registerRecyclingTelephone;
+    private final RegisterUserTelephone registerUserTelephone;
+
+    public TelephoneController(ChangeTelephone changeTelephone,
+                               DeleteTelephone deleteTelephone,
+                               FindAllTelephones findAllTelephones,
+                               FindAllTelephonesByOrganizationId findAllTelephonesByOrganizationId,
+                               FindTelephoneById findTelephoneById,
+                               FindTelephoneByRecyclingBusinessId findTelephoneByRecyclingBusinessId,
+                               FindTelephoneByUserId findTelephoneByUserId,
+                               RegisterRecyclingTelephone registerRecyclingTelephone,
+                               RegisterUserTelephone registerUserTelephone) {
+        this.changeTelephone = changeTelephone;
+        this.deleteTelephone = deleteTelephone;
+        this.findAllTelephones = findAllTelephones;
+        this.findAllTelephonesByOrganizationId = findAllTelephonesByOrganizationId;
+        this.findTelephoneById = findTelephoneById;
+        this.findTelephoneByRecyclingBusinessId = findTelephoneByRecyclingBusinessId;
+        this.findTelephoneByUserId = findTelephoneByUserId;
+        this.registerRecyclingTelephone = registerRecyclingTelephone;
+        this.registerUserTelephone = registerUserTelephone;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TelephoneOutput>> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+        return ResponseEntity.ok(findAllTelephones.execute(page, size));
+    }
+}
